0ff1b70121efec38a9ef87fb100af6929bd80c6a
josselin <obarayego@gmail.com>
2021-05-19 09:59:34 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const test: string = '123';
 
-console.log(test);
+for (let i: number = 1; i <= 100; i++) {
+  let first: boolean = true;
+
+  for (let j: number = i - 1; j > 1; j--) {
+    if (i % j === 0) {
+      first = false;
+    }
+  }
+  if (first === true) {
+    document.write(`${i} `);
+  }
+}
